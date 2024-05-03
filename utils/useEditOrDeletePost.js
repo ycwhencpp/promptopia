d990d3565d07ed17f9818402ac03e12ce4ad9b89
ycwhencpp <anurag.jay1002@gmail.com>
2024-05-03 21:36:15 +0530

    Update useEditOrDeletePost.js

--- utils/useEditOrDeletePost.js
+++ utils/useEditOrDeletePost.js
@@ -1,4 +1,4 @@
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 const useEditOrDeletePost = (userPosts, setUserPosts) => {
   const router = useRouter();
   const handelEdit = async (post) => {
